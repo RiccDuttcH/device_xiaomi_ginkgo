a65d14b636e64403d9f85dfe2c2a25f9095ab0ee
xmxrtyx <garikapatiamartya@gmail.com>
2020-11-21 08:49:54 +0530

    Bring Derpfest to Redmi Note 8/8T

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2020 ArrowOS
+# Copyright (C) 2020 DerpFest ROM
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_ginkgo.mk
+    $(LOCAL_DIR)/derp_ginkgo.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_ginkgo-user \
-    arrow_ginkgo-userdebug \
-    arrow_ginkgo-eng
+    derp_ginkgo-user \
+    derp_ginkgo-userdebug \
+    derp_ginkgo-eng
